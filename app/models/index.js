484e6c8b5a6a1c7e4963b0a8560f895514d61474
Igor Josiel <igorjosielvitalino@gmail.com>
2021-09-25 16:56:53 -0300

    feat: mounting the database data's structure like the columns, connections between tables, etc.

--- app/models/index.js
+++ app/models/index.js
@@ -18,4 +18,34 @@ const sequelize = new Sequelize(
             idle: config.pool.idle
         }
     }
-)
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("./user.model")(sequelize, Sequelize);
+db.role = require("./role.model")(sequelize, Sequelize);
+db.product = require("./product.model")(sequelize, Sequelize);
+db.request = require("./request.model")(sequelize, Sequelize);
+
+db.role.belongsToMany(db.user, {
+    through: "user_roles",
+    foreignKey: "roleId",
+    otherKey: "userId"
+});
+
+db.user.belongsToMany(db.role, {
+    through: "user_roles",
+    foreignKey: "userId",
+    otherKey: "roleId"
+});
+
+db.product.hasMany(db.request, {
+    foreignKey: "idProduct"
+});
+
+db.ROLES = ["admin", "moderator"];
+
+module.exports = db;
