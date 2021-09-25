d03240ad57f61e8c8a784929bdd8687769873cdf
Igor Josiel <igorjosielvitalino@gmail.com>
2021-09-25 16:08:47 -0300

    feat: creating the file to connect the database in the aplication.

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
+// app.use(cors)
+app.use(express.json);
+
+app.use(express.urlencoded({ extended: true }));
+
+// database
+const db = require("./app/models");
