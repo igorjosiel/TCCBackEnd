04ea2b777cb8fe40ca507b4cb6de9c1dfb380501
Igor Josiel <igorjosielvitalino@gmail.com>
2021-10-02 15:56:24 -0300

    feat: adding a function to verify if the user is an administrator.

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -25,6 +25,20 @@ verifyToken = (req, res, next) => {
     });
 }
 
-// isAdmin = (req, res, next) => {
+isAdmin = (req, res, next) => {
+    User.findByPK(req.userId).then(user => {
+        user.getRoles().then(roles => {
+            for (let i = 0; i < roles.length; i ++) {
+                if (roles[i].name === "admin") {
+                    next();
+                    return;
+                }
+            }
 
-// }
+            res.status(403).send({
+               message: "Requer acesso apenas pelo administrador!"
+            });
+            return;
+        });
+    });
+}
