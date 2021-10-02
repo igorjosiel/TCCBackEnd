cd69f8746ab600e42637ea3d9686b2d2fc4d2f82
Igor Josiel <igorjosielvitalino@gmail.com>
2021-10-02 14:37:37 -0300

    feat: creating the method to verify the user's token.

---
+++ app/middleware/authJwt.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+
+verifyToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+
+    if(!token) {
+        return res.status(403).send({
+            message: "Nenhum token foi provido!"
+        });
+    }
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if(err) {
+            return res.status(401).send({
+                message: "Usuário não autorizado!"
+            });
+        }
+
+        req.userId = decoded.id;
+
+        next();
+    });
+}
+
+// isAdmin = (req, res, next) => {
+
+// }
