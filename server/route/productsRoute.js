118addea6efc28c700ad4c2b3048145bbe3eb77e
Igor Josiel <igorjosielvitalino@gmail.com>
2021-11-20 17:00:54 -0300

    fix: fixing some more files and removing another ones that don't make sense in this case, in this project.

--- server/route/productsRoute.js
+++ server/route/productsRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const students = require('../service/studentsService');
+const products = require('../service/productsService');
 
 const message = "Ocorreu algo inesperado. Ou foi um erro de servidor!";
 
 router.get('/', async function(req, res, next) {
     try {
-        const { data, pagination, message, statusCode } = await students.getStudents(req.query);
+        const { data, pagination, message, statusCode } = await products.getProducts(req.query);
         
         res.status(statusCode).json({ data, pagination, message });
     }
@@ -18,43 +18,11 @@ router.get('/', async function(req, res, next) {
 
 router.get('/:id', async function(req, res, next) {
     try {
-        const { data, message, statusCode } = await students.getOneStudent(req.params.id);
+        const { data, message, statusCode } = await products.getOneProduct(req.params.id);
 
         res.status(statusCode).json({ data, message });
     }
     catch (err) {
-        res.status(500).json({ message });
-        next(err);
-    }
-});
-
-router.post('/', async function(req, res, next) {
-    try {
-        const { message, statusCode } = await students.createStudent(req.body);
-        
-        res.status(statusCode).json({ message });
-    } catch (err) {
-        res.status(500).json({ message });
-        next(err);
-    }
-});
-
-router.put('/:id', async function(req, res, next) {
-    try {
-        const { message, statusCode } = await students.updateStudent(req.params.id, req.body);
-        
-        res.status(statusCode).json({ message });
-    } catch (err) {
-        res.status(500).json({ message });
-        next(err);
-    }
-});
-
-router.delete('/:id', async function(req, res, next) {
-    try {
-        const { message, statusCode } = await students.removeStudent(req.params.id);
-        res.status(statusCode).json({ message });
-    } catch (err) {
         res.status(500).json({ message });
         next(err);
     }
